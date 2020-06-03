362b3010edc154d742521b779c7735b4266bd134
azj7319 <azj7319@163.com>
2020-06-03 15:42:17 +0800

    项目结构

---
+++ src/index/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { connect } from 'react-redux';
+
+
+function App(props){
+	return (<>ddd</>)
+}
+
+export default connect(
+	function mapStateToProps(state){},
+	function mapDispatchToProps(dispatch){}
+
+)(App);
